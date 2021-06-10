98139f98ec0542ed0f33b5ca87e25cf4facaf4c9
Ramon Oliveira <ramon.oliveira@activesoftconsultoria.com.br>
2021-06-09 23:50:57 -0300

    add rotas todo

--- routes.js
+++ routes.js
@@ -5,13 +5,19 @@ const { check, validationResult } = require('express-validator');
 const routes = express.Router();
 
 const UserController = require('./controllers/UserController');
-const QuestionController = require('./controllers/QuestionController')
+const QuestionController = require('./controllers/QuestionController');
 
-routes.post('/new-user/',UserController.create,  UserController.auth)
-routes.post('/auth/', UserController.auth)
-routes.get('/profile/', UserController.verifyJWT, UserController.profile)
-//routes.put('/profile/', UserController.verifyJWT, UserController.profile)
-routes.post('/question/', UserController.verifyJWT, QuestionController.create)
-routes.get('/question/:id', QuestionController.find)
+routes.post('/new-user/',UserController.create,  UserController.auth);
+routes.post('/auth/', UserController.auth);
+routes.get('/profile/', UserController.verifyJWT, UserController.profile);
+//routes.put('/profile/', UserController.verifyJWT, UserController.update);
+
+routes.post('/question/', UserController.verifyJWT, QuestionController.create);
+routes.get('/question/:id', QuestionController.find);
+//routes.put('/question/:id', UserController.verifyJWT, QuestionController.update);
+//routes.delete('/question/:id', UserController.verifyJWT, QuestionController.delete);
+
+//routes.post('/answers/', UserController.verifyJWT, AnswersController.create);
+//routes.get('/answers/', UserController.verifyJWT, AnswersController.find);
 
 module.exports = routes
